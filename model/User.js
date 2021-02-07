17ddbd660a47edba4e5e5c0c0863c5a035689033
thekogo <thekogo@gmail.com>
2021-02-07 12:26:44 +0700

    first commit

---
+++ model/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const User = new mongoose.Schema(
+	{
+		username: { type: String, require: true, unique: true },
+        password: { type: String, require: true},
+        created_at: { type: Date, default: Date.now }
+	},
+	{ collection: 'user' }
+)
+
+User.index({ username: 1 }, { unique: true });
+
+const UserModel = mongoose.model('User', User);
+module.exports = UserModel
